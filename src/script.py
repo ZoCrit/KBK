d58a58e3c91f0f830cb5225e50fa885da0fa7f11
zocrit <zozcristian@gmail.com>
2022-04-17 17:10:11 +0200

    Changed folder structure

--- src/script.py
+++ src/script.py
@@ -1,15 +1,12 @@
 import os
+from text_color_preset import prRed, prGreen, prYellow, prCyan
 
 kbk_config_file_path = '{}/Library/LaunchAgents/com.local.KeyRemapping.plist'.format(os.path.expanduser('~'))
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
-def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
-def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
-def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
-def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
 
 set_classic_immediate = '''
         hidutil property --set '{"UserKeyMapping":[
