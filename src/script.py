23bdec2f9a0ad2152643cb2071798047c3cae5db
zocrit <zozcristian@gmail.com>
2022-04-17 17:50:26 +0200

    Renamed variables

--- src/script.py
+++ src/script.py
@@ -1,6 +1,4 @@
-# from config_presets import kbk_config, classic_immediate, classic_persist, mid_immediate, mid_persist, left_immediate, left_persist, clear_immediate
-# from styling_presets import prRed, prGreen, prYellow, prCyan
-from presets import config_presets, styling_presets
+from presets import configs, styling
 import os
 
 def create_config():
@@ -10,23 +8,23 @@ def create_config():
   3) Left (F3-F4)\n\
   Which one would you like? ') or '1'
   if choice == 1 or choice == '1' or choice.startswith('c') or choice.startswith('C'):
-    os.system(classic_immediate)
-    config = open(kbk_config, mode = 'w')
-    config.write(classic_persist)
+    os.system(configs.classic_immediate)
+    config = open(configs.kbk_config, mode = 'w')
+    config.write(configs.classic_persist)
     config.close
   elif choice == 2 or choice == '2' or choice.startswith('m') or choice.startswith('M'):
-    os.system(mid_immediate)
-    config = open(kbk_config, mode = 'w')
-    config.write(mid_persist)
+    os.system(configs.mid_immediate)
+    config = open(configs.kbk_config, mode = 'w')
+    config.write(configs.mid_persist)
     config.close
   elif choice == 3 or choice == '3' or choice.startswith('l') or choice.startswith('L'):
-    os.system(left_immediate)
-    config = open(kbk_config, mode = 'w')
-    config.write(left_persist)
+    os.system(configs.left_immediate)
+    config = open(configs.kbk_config, mode = 'w')
+    config.write(configs.left_persist)
     config.close
 
 def check_existing_config():
-  if os.path.exists(kbk_config):
+  if os.path.exists(configs.kbk_config):
     ask_to_delete_config()
   else:
     create_config()
@@ -34,8 +32,8 @@ def check_existing_config():
 def ask_to_delete_config():
   choice = input('A KBK configuration already exists, do you want to delete it? [y/N]: \n') or 'N'
   if choice.startswith('y') or choice.startswith('Y'):
-    os.remove(kbk_config)
-    os.system(clear_immediate)
+    os.remove(configs.kbk_config)
+    os.system(configs.clear_immediate)
     print('The existing KBK configuration file has been deleted.')
   else:
     print('Exiting program')
@@ -44,7 +42,7 @@ check_existing_config()
 
 def main_menu():
   os.system('clear')
-  prCyan('''
+  styling.prCyan('''
     █████   ████ ███████████  █████   ████
    ░░███   ███░ ░░███░░░░░███░░███   ███░ 
     ░███  ███    ░███    ░███ ░███  ███   
